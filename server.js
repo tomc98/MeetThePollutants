c4ba5336670434fb26bd2a9d727cfad955a8698b
Tom <tomcsere@gmail.com>
2019-02-06 13:45:03 +1000

    multiple page support
    added privacy policy page

--- server.js
+++ server.js
@@ -1,57 +1,23 @@
 const express = require('express')
 const app = express()
 const fs = require('fs');
-let date = require('date-and-time');
 var $ = require('jquery')
-var data = require('./data/data.json')
+
+//------ Routes section ------\\
+var index = require('./routes/index');
+var terms = require('./routes/terms');
+var privacyPolicy = require('./routes/privacyPolicy');
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
+//----- Routing here -----\\
+app.use('/', index);
+app.use('/terms-and-conditions', terms);
+app.use('/privacy-policy', privacyPolicy);
 
-app.get('/', function(req, res) {
-    dataToEJS = {
-        data: data,
-        day: day = DayOfYear(new Date())
-    }
-    res.render('index', dataToEJS)
-})
+
 
 app.listen(3000, function() {
     console.log('Website is open on port 3000!')
 })
-
-//returns the days in a particular month
-function MonthToDays(month, isLeap) {
-    if (month == 2) {
-        if (isLeap) {
-            return 29;
-        } else {
-            return 28;
-        }
-    } else {
-        if (month > 7) {
-            if (month % 2) {
-                return 30
-            } else {
-                return 31
-            }
-        } else {
-            if (month % 2) {
-                return 31
-            } else {
-                return 30
-            }
-        }
-    }
-}
-
-//returns the day of the year.
-function DayOfYear(now) {
-    var total = 0;
-    for (var i = 1; i < date.format(now, 'M'); i++) {
-        total += parseInt(MonthToDays(i, date.isLeapYear(now)));
-    }
-    total += parseInt(date.format(now, 'D'));
-    return total;
-}

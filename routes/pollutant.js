3b3caff06ca2f294d25fda6b5a9c647d86f855c4
Tom <tomcsere@gmail.com>
2019-03-25 13:06:30 +1000

    Fixed PollutantList to work with database. started working with indvidual pollutant to work with database. (not complete)

--- routes/pollutant.js
+++ routes/pollutant.js
@@ -2,32 +2,31 @@ const express = require('express');
 const pollutant = express();
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'admin',
-    password: 'Password!1',
-    database: 'POLLUTANTS'
-});
-
-connection.connect();
-connection.query("SELECT * FROM `Pollutants`", function(err, rows, fields) {
-    if (err) throw err
-
-    //console.log(rows);
-});
-
-connection.end();
-
-//dynamic data is added here.
 var data = require('../data/data.json');
 
 //get home page
 pollutant.get('/', function(req, res) {
-    dataToEJS = {
-        data: data,
-        id: req.query.id
-    }
-    res.render('pollutant', dataToEJS)
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'admin',
+        password: 'Password!1',
+        database: 'POLLUTANTS'
+    });
+
+    connection.connect();
+    connection.query("SELECT * FROM `Pollutants` where Pollutant.ID = " + req.query.id + ";", function(err, rows, fields) {
+        if (err) throw err
+
+
+
+        //console.log(rows);
+        dataToEJS = {
+            data: data
+        }
+        res.render('pollutant', dataToEJS)
+        connection.end();
+    });
+
 })
 
 module.exports = pollutant;
--- routes/pollutantList.js
+++ routes/pollutantList.js
@@ -2,12 +2,10 @@ const express = require('express')
 const pollutantList = express()
 var mysql = require('mysql');
 
+
 //get home page
 pollutantList.get('/', function(req, res) {
-    //vars to use
     var data = [];
-    var rows = [];
-
     //connection to database
     var connection = mysql.createConnection({
         host: 'localhost',
@@ -19,25 +17,23 @@ pollutantList.get('/', function(req, res) {
     connection.connect();
     connection.query("SELECT * FROM `Pollutants`", function(err, rows, fields) {
         if (err) throw err
-        rows = rows;
+        for (var i = 0; i < rows.length; i++) {
+            data.push({
+                Id: rows[i].ID,
+                name: rows[i].Name,
+                featureImg: Buffer.from(rows[i].Img).toString('base64'),
+                description: rows[i].Description
+            });
+        }
+        dataToEJS = {
+            data: data,
+            req: req
+        }
+        res.render('pollutantList', dataToEJS)
+        connection.end();
     });
-    for (var i = 0; i < rows.length; i++) {
 
-        data.push({
-            Id: rows[i].ID,
-            name: rows[i].Name,
-            featureImg: "/polluntant/grosspollutants/grosspollutants.png",
-            description: rows[i].Description
-        });
-    }
-    console.log(data);
-    connection.end();
 
-    dataToEJS = {
-        data: data,
-        req: req
-    }
-    res.render('pollutantList', dataToEJS)
 })
 
 module.exports = pollutantList;
